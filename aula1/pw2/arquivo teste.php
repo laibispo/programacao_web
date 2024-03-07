2ab9458f3c4647d80d3783de9af207489a20d806
laibispo <162061683+laibispo@users.noreply.github.com>
2024-03-07 19:17:52 -0300

    aula1

---
+++ aula1/pw2/arquivo teste.php
@@ -0,0 +1,11 @@
+<?php
+print " outro arquivo ";
+//sempre usa o $ para desclara variavel//
+$nome1 = "lais";
+$nome2 = " bi ";
+$idade = 32;
+$peso = 68.4;
+print 'outro arquivo ' . 'php';
+print '<br>';
+print 'outro arquivo ' . $nome2;
+?>
